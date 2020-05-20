7968daa8f11c167386f233494e7c100b84461aaf
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-20 14:11:20 +0100

    feat: CommentsList
    finished client
    the basic setup of the 2 apis and frontend is done

--- client/src/PostsList.js
+++ client/src/PostsList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
+import CommentsList from "./CommentsList";
 
 const PostsList = () => {
   const [posts, setPosts] = useState({});
@@ -24,6 +25,7 @@ const PostsList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentsList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
