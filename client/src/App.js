a1272b433456b66c680e6d543090d04be8bfa976
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-20 13:48:03 +0100

    feat: PostsList
    PostsList component which fetches data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PostCreate from "./PostCreate";
+import PostsList from "./PostsList";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostsList />
     </div>
   );
 };
