babf4d045571b0b65abddc2f019056d92df0a1c7
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-25 11:39:27 +0100

    fix: minor fix
    destructured content property

--- comments-api/index.js
+++ comments-api/index.js
@@ -42,7 +42,7 @@ app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   if (type === "CommentModerated") {
-    const { id, postId, status } = data;
+    const { id, postId, content, status } = data;
 
     const comments = commentsByPostId[postId];
 
@@ -52,7 +52,7 @@ app.post("/events", async (req, res) => {
     comment.status = status;
 
     await axios.post("http://localhost:4005/events", {
-      type: CommentUpdated,
+      type: "CommentUpdated",
       data: {
         id,
         status,
