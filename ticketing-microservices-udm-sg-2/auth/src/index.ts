73a1f29db65c22ffb4b109b9d9f7458d18bcf8cb
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-29 12:54:39 +0100

    adding a new error

--- ticketing-microservices-udm-sg-2/auth/src/index.ts
+++ ticketing-microservices-udm-sg-2/auth/src/index.ts
@@ -6,15 +6,21 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
-app.use(errorHandler);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("listening on 3000");
