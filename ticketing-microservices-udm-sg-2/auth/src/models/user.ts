a4d31ecb602fb9f6049ebca9d0965a2146498baf
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-29 18:59:16 +0100

    store hash passwd

--- ticketing-microservices-udm-sg-2/auth/src/models/user.ts
+++ ticketing-microservices-udm-sg-2/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../utils/password";
 
 // describe required properties to create a new user
 interface UserAttrs {
@@ -27,6 +28,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
