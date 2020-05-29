996dca1e856acc5b750bbe61e1b4b50bd6c83e9b
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-29 11:53:57 +0100

    a potential fix/refactor

--- ticketing-microservices-udm-sg-2/auth/src/errors/database-connection-error.ts
+++ ticketing-microservices-udm-sg-2/auth/src/errors/database-connection-error.ts
@@ -1,8 +1,14 @@
 export class DatabaseConnectionError extends Error {
   reason = "Error connecting to the database";
+  statusCode = 500;
+
   constructor() {
     super();
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- ticketing-microservices-udm-sg-2/auth/src/errors/request-validation-error.ts
+++ ticketing-microservices-udm-sg-2/auth/src/errors/request-validation-error.ts
@@ -1,11 +1,19 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super();
 
     // when extending classes in ts
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
   }
 }
 
