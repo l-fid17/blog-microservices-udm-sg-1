6d2cc7d2bcfdfe8bbe1c6fe25d4704130d926b4c
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-29 23:34:58 +0100

    generate token

--- ticketing-microservices-udm-sg-2/auth/src/routes/signup.ts
+++ ticketing-microservices-udm-sg-2/auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -32,6 +34,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "aaa"
+    );
+
+    req.session = {
+      jwt: userJwt,
+      // isChanged: true,
+      // isNew: true,
+      // isPopulated: true,
+    };
 
     res.status(201).send(user);
   }
