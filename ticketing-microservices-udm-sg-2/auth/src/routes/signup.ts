754fa71b746cccc8f59dba404ca4f143a8d6b260
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-29 10:50:42 +0100

    validation

--- ticketing-microservices-udm-sg-2/auth/src/routes/signup.ts
+++ ticketing-microservices-udm-sg-2/auth/src/routes/signup.ts
@@ -1,9 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("response");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be of 4 to 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log("Creating user");
+
+    res.send({});
+
+    // new User({ email. password })
+  }
+);
 
 export { router as signupRouter };
