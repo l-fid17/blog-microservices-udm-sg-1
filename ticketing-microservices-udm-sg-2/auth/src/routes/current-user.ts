4ff0619632e415cd4acb74a96d007d2dacfef82a
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-30 19:20:35 +0100

    current user middleware
    augmenting types
    
    S9 - 173.

--- ticketing-microservices-udm-sg-2/auth/src/routes/current-user.ts
+++ ticketing-microservices-udm-sg-2/auth/src/routes/current-user.ts
@@ -1,19 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentuser: null });
-  }
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-    res.send({ currentuser: payload });
-  } catch (error) {
-    return res.send({ currentuser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentuser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
