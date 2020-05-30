72ffd1c9407a394634354620807ba876140c5d31
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-30 12:46:56 +0100

    signin and req validation

--- ticketing-microservices-udm-sg-2/auth/src/routes/signin.ts
+++ ticketing-microservices-udm-sg-2/auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../utils/password";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("response");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a valid password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const isMatch = await Password.compare(existingUser.password, password);
+
+    if (!isMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // req.session = { jwt: userJwt };
+    req.session.jwt = userJwt;
+
+    console.log(req.session.jwt);
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
