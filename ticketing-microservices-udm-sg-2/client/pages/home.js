2714fe8bbd44441ef19aaf33e91fdd7ca7bae5d2
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-05-31 18:55:35 +0100

    //

---
+++ ticketing-microservices-udm-sg-2/client/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Test from "../components/test";
+
+const home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+        <Test />
+    </div>
+  );
+};
+
+export default home;
