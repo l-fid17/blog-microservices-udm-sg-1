80fdf1d14f70c28bda03a994aa8b705b573e4da6
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-01 03:32:02 +0100

    cross namespace service communication
    S11 - 213.

--- ticketing-microservices-udm-sg-2/client/pages/index.js
+++ ticketing-microservices-udm-sg-2/client/pages/index.js
@@ -1,11 +1,35 @@
 import React from "react";
+import axios from "axios";
 
-const index = () => {
+const index = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div>
       <h1>Index</h1>
     </div>
   );
+};
+
+index.getInitialProps = async () => {
+  //if we are on the server
+  if (typeof window === "undefined") {
+    // route the req to nginx
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
 };
 
 export default index;
