d5bb07bf00e124333e55099485096a46e3f2820f
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-01 16:15:04 +0100

    move getInitialProps

--- ticketing-microservices-udm-sg-2/client/pages/_app.js
+++ ticketing-microservices-udm-sg-2/client/pages/_app.js
@@ -1,5 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({Component, pageProps}) => {
-    return <Component {...pageProps} />
-}
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <header>Header</header>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async ({ ctx }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
+
+export default AppComponent;
