43c2e7a1e08ac0ec4c6683ca8b721a07ed04fcb2
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-02 13:08:56 +0100

    migrate common logic to npm

--- ticketing-microservices-udm-sg-2/auth/src/routes/signin.ts
+++ ticketing-microservices-udm-sg-2/auth/src/routes/signin.ts
@@ -3,8 +3,7 @@ import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@sg-udemy-gittix/common";
 import { Password } from "../utils/password";
 
 const router = express.Router();
