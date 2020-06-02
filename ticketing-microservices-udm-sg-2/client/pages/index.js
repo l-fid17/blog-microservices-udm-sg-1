6d52816cc8584ab52abb01a5348ca05a2df33504
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-02 12:07:47 +0100

    re-enable ssr on component

--- ticketing-microservices-udm-sg-2/client/pages/index.js
+++ ticketing-microservices-udm-sg-2/client/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import buildClient from "../api/build-client";
+import buildClient from "../api/build-client";
 
 const index = ({ currentUser }) => {
   return currentUser ? <h1>Signed in</h1> : <h1>Not signed in</h1>;
 };
 
-// index.getInitialProps = async (context) => {
-//   const client = buildClient(context);
-//   const { data } = await client.get("/api/users/currentuser");
-//   return data;
-// };
+index.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
 
 export default index;
