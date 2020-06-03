ded579a0b8f0fd11ac0707a8f6eb7f9bf14477bf
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-03 14:09:34 +0100

    test body of incoming requests

--- ticketing-microservices-udm-sg-2/tickets/src/routes/__tests__/new.test.ts
+++ ticketing-microservices-udm-sg-2/tickets/src/routes/__tests__/new.test.ts
@@ -22,14 +22,40 @@ it("does not return 401 if the user is signed in", async () => {
 
 it("returns an error if title is invalid", async () => {
   await request(app)
-      .post('/api/tickets')
-      .set('Cookie', global.signin())
-      .send({
-        title: '',
-        price: 10
-      })
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
 });
 
-it("returns an error if price is invalid", async () => {});
+it("returns an error if price is invalid", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "title",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "title",
+    })
+    .expect(400);
+});
 
 it("creates a ticket if inputs are valid", async () => {});
