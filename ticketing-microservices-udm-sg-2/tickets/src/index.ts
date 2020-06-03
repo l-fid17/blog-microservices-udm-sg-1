69e4c5d8c33800e507421dd9602dc6c2ec2d3156
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-03 13:21:56 +0100

    tickets deployment

---
+++ ticketing-microservices-udm-sg-2/tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  try {
+    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("listening on 3000");
+  });
+};
+
+start();
