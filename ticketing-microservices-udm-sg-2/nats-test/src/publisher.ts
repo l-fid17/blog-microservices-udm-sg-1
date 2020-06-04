a83abdd7577999386abce1acacee7608e019c706
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-04 11:43:12 +0100

    publish an event
    S14 - 267.

--- ticketing-microservices-udm-sg-2/nats-test/src/publisher.ts
+++ ticketing-microservices-udm-sg-2/nats-test/src/publisher.ts
@@ -7,4 +7,16 @@ const stan = nats.connect("ticketing", "abc", {
 
 stan.on("connect", () => {
   console.log("publisher connected to nats");
+
+  const data = {
+    id: "123",
+    title: "title",
+    price: 20,
+  };
+
+  const dataString = JSON.stringify(data);
+
+  stan.publish("ticket:created", dataString, () => {
+    console.log(`published ${dataString}`);
+  });
 });
