1152a71bef6a9031279e193274e54248253804a0
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-05 14:33:56 +0100

    enforce listener subject
    S15 - 289.

--- ticketing-microservices-udm-sg-2/nats-test/src/events/base-listener.ts
+++ ticketing-microservices-udm-sg-2/nats-test/src/events/base-listener.ts
@@ -1,15 +1,15 @@
 import { Message, Stan } from "node-nats-streaming";
-import {Subjects} from "./subjects";
+import { Subjects } from "./subjects";
 
 interface Event {
-  subject: Subjects
-  data: any
+  subject: Subjects;
+  data: any;
 }
 
 export abstract class Listener<T extends Event> {
-  abstract subject: T['subject'];
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
-  abstract onMessage(data: T['data'], msg: Message): void;
+  abstract onMessage(data: T["data"], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
--- ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-listener.ts
+++ ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-listener.ts
@@ -1,7 +1,7 @@
-import {Message} from "node-nats-streaming";
-import {Listener} from "./base-listener";
-import {TicketCreateEvent} from "./ticket-create-event";
-import {Subjects} from "./subjects";
+import { Message } from "node-nats-streaming";
+import { Listener } from "./base-listener";
+import { TicketCreateEvent } from "./ticket-create-event";
+import { Subjects } from "./subjects";
 
 export class TicketCreatedListener extends Listener<TicketCreateEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
