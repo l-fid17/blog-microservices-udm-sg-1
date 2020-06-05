81e8e3abf0eb2dd8c2c7f781f981e532c9449c07
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-05 14:33:38 +0100

    enforce listener subject
    S15 - 289.

--- ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-listener.ts
+++ ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,10 @@
-import { Message } from "node-nats-streaming";
-import { Listener } from "./base-listener";
+import {Message} from "node-nats-streaming";
+import {Listener} from "./base-listener";
+import {TicketCreateEvent} from "./ticket-create-event";
+import {Subjects} from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreateEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
   onMessage(data: any, msg: Message) {
