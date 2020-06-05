3cb8b398f2bbe79682344a92f3e3c37f2018950c
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-05 14:50:23 +0100

    custom publisher
    S15 - 293.

--- ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-publisher.ts
+++ ticketing-microservices-udm-sg-2/nats-test/src/events/ticket-created-publisher.ts
@@ -1,7 +1,7 @@
-import {Publisher} from "./base-publisher";
-import {TicketCreateEvent} from "./ticket-create-event";
-import {Subjects} from "./subjects";
+import { Publisher } from "./base-publisher";
+import { TicketCreateEvent } from "./ticket-create-event";
+import { Subjects } from "./subjects";
 
 export class TicketCreatedPublisher extends Publisher<TicketCreateEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
 }
