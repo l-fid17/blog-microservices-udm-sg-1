4a6aa02e552551372dfa0736d108013041f70655
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-07 00:02:01 +0100

    validation and auth
    S17 - 321.

--- ticketing-microservices-udm-sg-2/orders/src/routes/new.ts
+++ ticketing-microservices-udm-sg-2/orders/src/routes/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@sg-udemy-gittix/common";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketID")
+      .not()
+      .isEmpty()
+      //   // test if the id is valid
+      //   // however this is a big assumption about the tickets service which is not suggested in real apps
+      // .custom((input:string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("ticketID must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
