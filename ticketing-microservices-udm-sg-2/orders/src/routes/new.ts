f142a705f36c3089e063394f365bb07393ab6a9c
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-07 12:22:16 +0100

    order creation logic
    S17 - 329.

--- ticketing-microservices-udm-sg-2/orders/src/routes/new.ts
+++ ticketing-microservices-udm-sg-2/orders/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@sg-udemy-gittix/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@sg-udemy-gittix/common";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -18,6 +26,39 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketID } = req.body;
+
+    // find the ticket the user is trying to order
+    const ticket = await Ticket.findById(ticketID);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // make sure the ticket has not been reserved yet
+    // run query to look at all orders
+    // find an order where the ticket is the one we just found above and the order status is not cancelled
+    // if we find one it means the ticket has been reserved
+    const existingOrder = await Order.findOne({
+      ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // calculate the expiration for the order
+
+    // build the order and save it to db
+
+    // publish an event saying that an order has been created
+
     res.send({});
   }
 );
