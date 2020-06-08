2bd21ac83de76529a899a95d444152541ac6ea6b
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-08 10:58:38 +0100

    fetch a single order
    S17 - 338.

--- ticketing-microservices-udm-sg-2/orders/src/routes/show.ts
+++ ticketing-microservices-udm-sg-2/orders/src/routes/show.ts
@@ -1,9 +1,30 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sg-udemy-gittix/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderID", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderID",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    // should check for a valid id first, then ->
+    const order = await Order.findById(req.params.orderID).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userID !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
