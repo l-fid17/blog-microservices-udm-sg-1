0bb75f77d8e603bb2e860f2949e30659a33940b8
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-08 12:17:48 +0100

    test events
    S17 - 347.

--- ticketing-microservices-udm-sg-2/orders/src/routes/__tests__/delete.test.ts
+++ ticketing-microservices-udm-sg-2/orders/src/routes/__tests__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("updates the status to Cancelled", async () => {
   // create a ticket
@@ -32,4 +33,28 @@ it("updates the status to Cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order cancelled event");
+it("emits an order cancelled event", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "test",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // build an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketID: ticket.id })
+    .expect(201);
+
+  // patch the order to cancelled
+  await request(app)
+    .patch(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
