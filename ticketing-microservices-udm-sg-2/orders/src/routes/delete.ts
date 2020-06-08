aff00c04e266b02900082df007994ecbc696f066
Luca Fidanza <f1d.pro.luca@gmail.com>
2020-06-08 11:14:04 +0100

    cancel the order
    S17 - 340.

--- ticketing-microservices-udm-sg-2/orders/src/routes/delete.ts
+++ ticketing-microservices-udm-sg-2/orders/src/routes/delete.ts
@@ -1,9 +1,34 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sg-udemy-gittix/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderID", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.patch(
+  "/api/orders/:orderID",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderID } = req.params;
+
+    const order = await Order.findById(orderID);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userID !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(200).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
