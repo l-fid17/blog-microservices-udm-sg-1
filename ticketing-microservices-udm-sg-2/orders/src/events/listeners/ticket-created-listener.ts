2d729588eceecb56e6fd9b4d723f9c11fe470c83
l-f1d <f1d.pro.luca@gmail.com>
2020-06-09 00:21:41 +0100

    standardise id
    S19 - 353.

--- ticketing-microservices-udm-sg-2/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing-microservices-udm-sg-2/orders/src/events/listeners/ticket-created-listener.ts
@@ -4,10 +4,20 @@ import {
   TicketCreatedEvent,
 } from "@sg-udemy-gittix/common";
 import { Message } from "node-nats-streaming";
+import {queueGroupName} from "./queue-group-name";
+import {Ticket} from "../../models/ticket";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
+  queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const {id, title, price} = data
+    const ticket = Ticket.build({
+      id, title, price
+    })
+    await ticket.save()
+
+    msg.ack()
+  }
 }
