113471746e1f3d26f8c5be6c8e2b210969cf4728
l-f1d <f1d.pro.luca@gmail.com>
2020-06-12 13:03:26 +0100

    test listener
    S19 - 384.

---
+++ ticketing-microservices-udm-sg-2/tickets/src/events/listeners/__tests__/order-created-listener.test.ts
@@ -0,0 +1,58 @@
+import { OrderCreatedListener } from "../order-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { Ticket } from "../../../models/ticket";
+import { OrderCreatedEvent, OrderStatus } from "@sg-udemy-gittix/common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  // create a listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = await Ticket.build({
+    title: "test",
+    price: 20,
+    userID: "fakeuserid",
+  });
+  await ticket.save();
+
+  // create fake data
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userID: "fakeuserid",
+    expiresAt: "whatevertime",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // create a fake msg
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, ticket, msg };
+};
+
+it("sets the userID of the ticket", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderID).toEqual(data.id);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
