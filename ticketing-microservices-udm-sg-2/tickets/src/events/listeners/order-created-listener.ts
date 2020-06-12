3969490abb2e1dfa28f7c0521da657acb9642a67
l-f1d <f1d.pro.luca@gmail.com>
2020-06-12 23:34:53 +0100

    publish while listening
    S19 - 387.

--- ticketing-microservices-udm-sg-2/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing-microservices-udm-sg-2/tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -25,6 +26,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // save the ticket
     await ticket.save();
+
+    // emit event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+      userID: ticket.userID,
+      orderID: ticket.orderID,
+    });
 
     // ack the message
     msg.ack();
