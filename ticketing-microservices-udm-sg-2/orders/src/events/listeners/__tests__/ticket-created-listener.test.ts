1e7613dffe4cf678b8e6ac9b2dbd56ef4f76192f
l-f1d <f1d.pro.luca@gmail.com>
2020-06-12 12:00:28 +0100

    test listener
    S19 - 372.

---
+++ ticketing-microservices-udm-sg-2/orders/src/events/listeners/__tests__/ticket-created-listener.test.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { TicketCreatedListener } from "../ticket-created-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { TicketCreatedEvent } from "@sg-udemy-gittix/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new TicketCreatedListener(natsWrapper.client);
+
+  // create a fake data event
+  const data: TicketCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    title: "test",
+    price: 10,
+    userID: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("creates and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with data obj & msg obj
+  await listener.onMessage(data, msg);
+
+  // assert ticket creation
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+
+it("acks the message", async () => {
+  // call the onMessage function with data obj & msg obj
+  // assert ack is called
+});
