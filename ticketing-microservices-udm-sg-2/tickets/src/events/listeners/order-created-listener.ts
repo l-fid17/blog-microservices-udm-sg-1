a280911b6aae893148834cdc8fa56d1249646e15
l-f1d <f1d.pro.luca@gmail.com>
2020-06-12 12:38:34 +0100

    build listener
    S19 - 380.

---
+++ ticketing-microservices-udm-sg-2/tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,9 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@sg-udemy-gittix/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
