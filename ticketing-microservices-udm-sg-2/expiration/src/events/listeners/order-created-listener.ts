fd3eb95b3a74744771b90c82f9ea91b16a80cc08
l-f1d <f1d.pro.luca@gmail.com>
2020-06-13 20:25:43 +0100

    queue a job
    S20 - 401.

--- ticketing-microservices-udm-sg-2/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing-microservices-udm-sg-2/expiration/src/events/listeners/order-created-listener.ts
@@ -1,9 +1,16 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@sg-udemy-gittix/common";
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderID: data.id,
+    });
+
+    msg.ack();
+  }
 }
